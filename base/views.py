826f0cc35f6831f4ae485ae2f1f55125e056953d
Muhtasim Ibteda Shochcho <sho25100@gmail.com>
2024-02-04 18:48:23 +0600

    P

--- base/views.py
+++ base/views.py
@@ -9,6 +9,6 @@ rooms = [
 
 def home(request):
     context = {'rooms':rooms}
-    return render(request, 'home.html', context)
-def room(request):
-    return render(request, 'room.html')
+    return render(request, 'base/home.html', context)
+def room(request, pk):
+    return render(request, 'base/room.html')
